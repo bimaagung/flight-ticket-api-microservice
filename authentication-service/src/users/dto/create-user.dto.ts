83e3eb05b17c337c895cca95159e2172c3ee2ec5
bimaagung <bimaagung.labs@gmail.com>
2023-04-20 19:21:19 +0700

    feat: add feature created user in authentication-service

--- authentication-service/src/users/dto/create-user.dto.ts
+++ authentication-service/src/users/dto/create-user.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  first_name: string;
+  last_name: string;
+  gender: number;
+  address: string;
+  email: string;
+  password: string;
+  admin: boolean;
+}
--- authentication-service/src/users/users.service.ts
+++ authentication-service/src/users/users.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const newUser = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(newUser);
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.usersRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.usersRepository.findOneBy({ id });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const user = await this.findOne(id);
+    return this.usersRepository.save({ ...user, ...updateUserDto });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    const user = await this.findOne(id);
+    return this.usersRepository.remove(user);
   }
 }
