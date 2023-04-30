5d16a636fd6a01640162bf1be9c7d49cd694b105
bimaagung <bimaagung.labs@gmail.com>
2023-04-30 13:15:06 +0700

    refactor: remove create user in user controller

--- authentication-service/src/users/users.controller.ts
+++ authentication-service/src/users/users.controller.ts
@@ -1,24 +1,10 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Put,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Body, Put, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.add(createUserDto);
-  }
 
   @Get()
   findAll() {
