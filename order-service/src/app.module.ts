e54cb1a0068131462ad5b431c6f0e4120b5fcf05
bimaagung <bimaagung.labs@gmail.com>
2023-05-09 18:01:55 +0700

    feat: created schema order

--- order-service/src/app.module.ts
+++ order-service/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
-import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), OrdersModule],
+  imports: [OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
